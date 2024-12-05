8ae786bda1b9a1cfa14f8662cb3432b187f7bc66
ilyas <ilyasistiqama@gmail.com>
2024-12-05 18:19:47 +0700

    [edit] favicon

--- src/pages/SectionAboutMe.jsx
+++ src/pages/SectionAboutMe.jsx
@@ -18,7 +18,7 @@ const SectionAboutMe = () => {
                     <div id="text">
                         <h2>ABOUT ME</h2>
                         <p>
-                            Hi everybody. Let me introduce myself, my name is Muh. Ilyas Istiqamah. M, but you can call me Ilyas. I come from Luwuk and currently live in Surabaya. I am 25 years old and graduated with a GPA of 3.62/4 and a Bachelor's degree in Information Engineering from the University of 17 August 1945 Surabaya with a S.Kom. I enjoy learning new things, especially in the field of programming.
+                            Hi everybody. Let me introduce myself, my name is Muh. Ilyas Istiqama. M, but you can call me Ilyas. I come from Luwuk and currently live in Surabaya. I am 25 years old and graduated with a GPA of 3.62/4 and a Bachelor's degree in Information Engineering from the University of 17 August 1945 Surabaya with a S.Kom. I enjoy learning new things, especially in the field of programming.
                         </p>
                         <p>
                             In addition to my programming skills, I have strong critical and creative thinking abilities,
